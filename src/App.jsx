8f9a622e2a34e3944e2d79296f7ec8da8b0dcad9
jatin <125881431+Jatin083@users.noreply.github.com>
2024-05-24 20:20:28 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import LandingPage from './components/LandingPage'
 import Mraquee from './components/Mraquee'
 import About from './components/About'
 import Eyes from './components/Eyes'
-import Features from './components/Featured'
+import Features from './components/Features'
 import Cards from './components/Cards'
 import Footer from './components/Footer'
 import LocomotiveScroll from 'locomotive-scroll';
