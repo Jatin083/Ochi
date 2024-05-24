a1cf401d70c9b65fdb5bc845162fab652159daf9
jatin <125881431+Jatin083@users.noreply.github.com>
2024-05-24 20:03:29 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import LandingPage from './components/LandingPage'
+import Mraquee from './components/Mraquee'
+import About from './components/About'
+import Eyes from './components/Eyes'
+import Features from './components/Featured'
+import Cards from './components/Cards'
+import Footer from './components/Footer'
+import LocomotiveScroll from 'locomotive-scroll';
+
+
+
+function App() {
+  const locomotiveScroll = new LocomotiveScroll();
+  return (
+    <div className='w-full min-h-screen text-white bg-zinc-900'>
+      <Navbar/>
+      <LandingPage/>
+      <Mraquee/>
+      <About/>
+      <Eyes/>
+      <Features/>
+      <Cards/>
+      <Footer/>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Mraquee.jsx
@@ -0,0 +1,22 @@
+import { motion } from 'framer-motion'
+import React from 'react'
+
+const Marquee = () => {
+
+  return (
+    <div data-scroll data-scroll-section data-scroll-speed="0.1" className='w-full py-20 rounded-tl-3xl rounded-tr-3xl bg-[#004D43]'>
+        <div className="text border-t-2 border-b-2 border-zinc-400 flex whitespace-nowrap overflow-hidden">
+
+            <motion.h1 initial={{x: 0}} animate={{x: "-100%"}} transition={{ease: "linear", repeat: Infinity, duration: 10}} className='text-[22vw] leading-none font-["Founders Grotesk_X-Condensed"] font-semibold uppercase -mb-[1vw] pt-1 pr-20'>We are ochi</motion.h1>
+
+            <motion.h1 initial={{x: 0}} animate={{x: "-100%"}} transition={{ repeat: Infinity, ease: "linear", duration: 10}} className='text-[22vw] leading-none font-["Founders Grotesk_X-Condensed"] font-semibold uppercase -mb-[4vw] pt-1 pr-20'>We are ochi</motion.h1>
+
+            <motion.h1 initial={{x: 0}} animate={{x: "-100%"}} transition={{ repeat: Infinity, ease: "linear", duration: 10}} className='text-[22vw] leading-none font-["Founders Grotesk_X-Condensed"] font-semibold uppercase -mb-[2vw] pt-1 pr-20'>We are ochi</motion.h1>
+
+ 
+        </div>
+    </div>
+  )
+}
+
+export default Marquee
